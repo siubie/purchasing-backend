a2b9259e1796f244ccdfa03095b272385b382675
Disyam Adityana <disyam.adityana@gmail.com>
2015-03-30 09:21:03 +0700

    buat controller untuk modul master waste

---
+++ app/controllers/waste.js
@@ -0,0 +1,58 @@
+var express = require('express'),
+    router = express.Router(),
+    mongoose = require('mongoose'),
+    waste = mongoose.model('waste');
+
+module.exports = function(app) {
+    app.use('/purchasing', router);
+};
+
+router.route('/waste')
+    .get(function(req, res, next) {
+        waste.find(function(err, result) {
+            if (err)
+                return next(err);
+            else
+                res.json(result);
+        });
+    })
+    .post(function(req, res, next) {
+        waste.create(req.body, function(err, result) {
+            if (err)
+                return next(err);
+            else
+                res.json(result);
+        });
+    });
+
+router.route('/waste/:id')
+    .get(function(req, res, next) {
+        waste.findOne({
+            'kode': req.params.id
+        }, function(err, result) {
+            if (err)
+                return next(err);
+            else
+                res.json(result);
+        });
+    })
+    .put(function(req, res, next) {
+        waste.findOneAndUpdate({
+            'kode': req.params.id
+        }, req.body, function(err, result) {
+            if (err)
+                return next(err);
+            else
+                res.json(result);
+        });
+    })
+    .delete(function(req, res, next) {
+        waste.findOneAndRemove({
+            'kode': req.params.id
+        }, function(err, result) {
+            if (err)
+                return next(err);
+            else
+                res.json(result);
+        });
+    });
